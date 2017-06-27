183ac795852dc4494067aa2b975ba03593ee6db7
novas_ghost <tongqinw@163.com>
2017-06-27 17:00:53 +0800

    完成创建订单

--- src/com/servlet/makeOrderForm.java
+++ src/com/servlet/makeOrderForm.java
@@ -2,10 +2,16 @@ package com.servlet;
 
 import com.Login.Handler.MyJsonParser;
 import com.Login.Sessions.SessionManager;
+import com.beans.book_brief;
+import com.beans.orderForm;
+import com.beans.storage_book;
 import com.dao.abstruct_dao;
-import com.dao.book_brw_dao;
+import com.dao.cart_dao;
+import com.dao.orderForm_dao;
 import com.dao.user_dao;
 import com.google.gson.JsonObject;
+import com.util.OF_util;
+import net.sf.json.JSONArray;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,10 +22,10 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
- * Created by hasee on 2017/6/26.
+ * Created by hasee on 2017/6/27.
  */
-@WebServlet(name = "cartHandler")
-public class boReBook extends HttpServlet{
+@WebServlet(name = "makeOrderForm")
+public class makeOrderForm  extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //开头
         response.setContentType("text/html;charset=UTF-8");
@@ -31,23 +37,32 @@ public class boReBook extends HttpServlet{
         String retString="failure";
 
         //取参数
-        String operation = request.getParameter("operation");//borrow,return
-        String barcode = request.getParameter("barcode");
+        String barcode1 = request.getParameter("barcode1");
+        String barcode2 = request.getParameter("barcode2");
         String unionid = request.getParameter("unionid");
 
         int userid = user_dao.getUserByUnionId(unionid).getUserid();
-        String orderid="123";
-        switch (operation) {
-            case "borrow":
-                if (book_brw_dao.borrowBook(unionid,orderid,barcode,true))
-                    retString="success";
-                break;
-            case "return":
-                if (book_brw_dao.returnBook(orderid,barcode,true))
-                    retString="success";
-                break;
-        }
 
+        //book_brief[] books= cart_dao.getbooks(userid);
+        //JSONArray book_brief_json = JSONArray.fromObject(books);
+        //retString = book_brief_json.toString();
+        if (barcode1!=null){
+            //至少有一本书
+            String orderid= OF_util.getNewOrderid();
+            OF_util of_util =  new OF_util(unionid,orderid);
+            storage_book book1 = new storage_book();
+            book1.setBook_id(barcode1);
+            of_util.add(book1);
+            if (barcode2!=null){
+                storage_book book2 = new storage_book();
+                book2.setBook_id(barcode2);
+                of_util.add(book2);
+            }
+            orderForm OrderForm = of_util.toOrderForm();
+            orderForm_dao OFD = new orderForm_dao(OrderForm);
+            if (!OFD.add()) retString="create order form failure";
+            else retString=orderid;
+        }
 
         out.write(retString);
         //结尾
@@ -68,18 +83,18 @@ public class boReBook extends HttpServlet{
         //取参数
         JsonObject jsonObject = MyJsonParser.String2Json(CreateSessionServlet.getBody(request));
         String unionid = null;
-        String operation = null;
-        String barcode = null;
+        String barcode1 = null;
+        String barcode2 = null;
         String session_id = null;
 
         try {
-            barcode = jsonObject.get("barcode").getAsString();
-            operation = jsonObject.get("operation").getAsString();
+            barcode1 = jsonObject.get("barcode1").getAsString();
+            barcode2 = jsonObject.get("barcode2").getAsString();
             session_id = jsonObject.get("session_id").getAsString();
         } catch (Exception e){
             out.write("failure: error json type");
             out.flush();
-            int i=0;
+            int j=1;
             out.close();
             response.flushBuffer();
             return;
@@ -87,19 +102,28 @@ public class boReBook extends HttpServlet{
         unionid = SessionManager.getInstance().getUser(session_id).getOpenID();
 
         int userid = user_dao.getUserByUnionId(unionid).getUserid();
-        String orderid="123";
-        switch (operation) {
-            case "borrow":
-                if (book_brw_dao.borrowBook(unionid,orderid,barcode,true))
-                    retString="success";
-                    int i=0;
-                break;
-            case "return":
-                if (book_brw_dao.returnBook(orderid,barcode,true))
-                    retString="success";
-                break;
-        }
 
+        //book_brief[] books= cart_dao.getbooks(userid);
+        //JSONArray book_brief_json = JSONArray.fromObject(books);
+        //retString = book_brief_json.toString();
+        if (barcode1!=null){
+            //至少有一本书
+            String orderid= OF_util.getNewOrderid();
+            OF_util of_util =  new OF_util(unionid,orderid);
+            storage_book book1 = new storage_book();
+            book1.setBook_id(barcode1);
+            of_util.add(book1);
+            int j;
+            if (barcode2!=null){
+                storage_book book2 = new storage_book();
+                book2.setBook_id(barcode2);
+                of_util.add(book2);
+            }
+            orderForm OrderForm = of_util.toOrderForm();
+            orderForm_dao OFD = new orderForm_dao(OrderForm);
+            if (!OFD.add()) retString="create order form failure";
+            else retString=orderid;
+        }
 
         out.write(retString);
         //结尾
